de6f616f538a20fff7572b21267078efffadb854
onishitaisei <taisei.developer@gmail.com>
2023-05-23 06:43:54 +0900

    practice4

--- SampleProject/ContentView.swift
+++ SampleProject/ContentView.swift
@@ -8,46 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isOn = true;
     
     var body: some View {
         VStack {
-            
-            ZStack {
-                Rectangle().foregroundColor(.yellow).frame(width: 300, height: 300)
-                
-                VStack {
-                    HStack {
-                        Rectangle().frame(width: 60, height: 20)
-                        ZStack {
-                            Rectangle().foregroundColor(.gray).frame(width: 90, height: 90)
-                            Rectangle().foregroundColor(.white).frame(width: 70, height: 70)
-                            Rectangle().frame(width: 20, height: 20)
-                        }
-                        ZStack {
-                            Rectangle().foregroundColor(.gray).frame(width: 90, height: 90)
-                            Rectangle().foregroundColor(.white).frame(width: 70, height: 70)
-                            Rectangle().frame(width: 20, height: 20)
-                        }
-                        
-                        Rectangle().frame(width: 60, height: 20)
-                    }
-                    ZStack {
-                        Rectangle().frame(width: 70, height: 40)
-                        VStack {
-                            
-                            Rectangle().foregroundColor(.white).frame(width: 60, height: 10)
-                            Rectangle().foregroundColor(.red).frame(width: 60, height: 10)
-                        }
-                        
-                    }
-                }
-                
-            }
-            Rectangle().foregroundColor(.blue).frame(width: 300, height: 100)
-            HStack {
-                Rectangle().frame(width: 70, height: 30)
-                Rectangle().frame(width: 70, height: 30)
-            }
+            Toggle("スイッチ", isOn: $isOn)
         }
     }
     

bfbb1f3ea695c6c60c493934779f71cfd3bbd28d
onishitaisei <taisei.developer@gmail.com>
2023-05-23 23:57:26 +0900

    bug fix

--- SampleProject/ContentView.swift
+++ SampleProject/ContentView.swift
@@ -8,7 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var randomNumber = 1
+    @State private var randomNumber = 1
+    @State private var timer: Timer?
+    @State private var isRolling = false
     
     var body: some View {
         VStack {
@@ -20,19 +22,33 @@ struct ContentView: View {
                 .padding()
             Spacer()
             Button {
-                print("ボタンが押されたよ")
-                randomNumber = Int.random(in: 1...6)
+                playDice()
             } label: {
                 Text("サイコロをふる")
                     .padding()
                     .background(.orange)
                     .foregroundColor(.black)
                     .cornerRadius(10)
-            }
+            }.disabled(isRolling)
             Spacer()
         }
     }
     
+    private func playDice() {
+        isRolling = true
+        print("ボタンが押されたよ")
+        // 0.1秒ごとにタイマーが発火
+        timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { _ in
+            randomNumber = Int.random(in: 1...6)
+        }
+        // 0.5秒後にタイマーを止める
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            timer?.invalidate()
+            timer = nil
+            isRolling = false
+        }
+    }
+ 
 }
 
 struct ContentView_Previews: PreviewProvider {

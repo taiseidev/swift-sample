4de530187c567710fd83b5d94198ffc7d63d985d
onishitaisei <taisei.developer@gmail.com>
2023-05-29 22:05:28 +0900

    Cut out the code

--- SampleProject/ContentView.swift
+++ SampleProject/ContentView.swift
@@ -14,29 +14,70 @@ struct TaskData: Identifiable {
 }
 
 struct ContentView: View {
-    @State var taskData = [
-        TaskData(title: "ジョギングする", completed: false),
-        TaskData(title: "お花に水をやる", completed: false),
-        TaskData(title: "部屋の掃除をする", completed: false),
-        TaskData(title: "本を読む", completed: false)
-    ]
+    var body: some View {
+        VStack {
+            FaceView()
+            PantsView()
+            FooterView()
+        }
+    }
+}
+
+struct FaceView: View {
+    var body: some View {
+    ZStack {
+        RectangleView(color: .yellow, width: 300, height: 300)
+        VStack {
+        HStack {
+            RectangleView(color: .black, width: 60, height: 20)
+            ZStack {
+            RectangleView(color: .gray, width: 90, height: 90)
+            RectangleView(color: .white, width: 70, height: 70)
+            RectangleView(color: .black, width: 20, height: 20)
+            }
+            ZStack {
+            RectangleView(color: .gray, width: 90, height: 90)
+            RectangleView(color: .white, width: 70, height: 70)
+            RectangleView(color: .black, width: 20, height: 20)
+            }
+            RectangleView(color: .black, width: 60, height: 20)
+        }
+        ZStack {
+            RectangleView(color: .black, width: 70, height: 40)
+            VStack {
+            RectangleView(color: .white, width: 60, height: 10)
+            RectangleView(color: .red, width: 60, height: 10)
+            }
+        }
+        }
+    }
+    }
+}
+
+struct PantsView: View {
+    var body: some View {
+        return RectangleView(color: .blue, width: 300, height: 100)
+    }
+}
+
+struct FooterView: View {
+    var body: some View {
+        return HStack {
+            RectangleView(color: .black, width: 70, height: 30)
+            RectangleView(color: .black, width: 70, height: 30)
+        }
+    }
+}
+
+struct RectangleView: View {
+    let color: Color
+    let width: CGFloat
+    let height: CGFloat
     
     var body: some View {
-        NavigationStack {
-            List(0..<taskData.count, id:\.self) { index in
-                Button {
-                    taskData[index].completed.toggle()
-                } label: {
-                    HStack {
-                        taskData[index].completed ?
-                        Image(systemName: "checkmark.circle.fill") :
-                        Image(systemName: "circle")
-                        Text(taskData[index].title)
-                    }.foregroundColor(.primary)
-                }
-                
-            }.navigationTitle("ToDoリスト")
-        }
+    Rectangle()
+        .foregroundColor(color)
+        .frame(width: width, height: height)
     }
 }
 

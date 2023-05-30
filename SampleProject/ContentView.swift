8ebe0834d7fa84800ed39e98fbc4b1ffb0adc036
onishitaisei <taisei.developer@gmail.com>
2023-05-30 19:39:37 +0900

    Review @State and @Biding

--- SampleProject/ContentView.swift
+++ SampleProject/ContentView.swift
@@ -1,53 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    let symbols = [
-        "pencil",
-        "pencil.circle",
-        "pencil.circle.fill",
-        "pencil.slash",
-        "square.and.pencil",
-        "rectangle.and.pencil.and.ellipsis",
-        "highlighter",
-        "pencil.and.outline",
-        "pencil.tip",
-        "pencil.tip.crop.circle",
-        "pencil.tip.crop.circle.badge.plus",
-        "pencil.tip.crop.circle.badge.minus",
-        "pencil.tip.crop.circle.badge.arrow.forward",
-        "lasso",
-        "lasso.and.sparkles",
-        "trash",
-        "trash.fill",
-        "trash.circle",
-        "trash.circle.fill",
-        "trash.square",
-        "trash.square.fill",
-        "trash.slash",
-    ]
-    
-    let rows: [GridItem] = Array(repeating: .init(.flexible()), count: 1)
+    @State var inputText = ""
+    @State var isShowInputView = false
     
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: rows) {
-                ForEach(symbols, id: \.self) { symbol in
-                    Image(systemName: symbol)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 50, height: 50)
-                        .padding(20)
-                        .background(.white)
-                        .foregroundColor(.black)
-                        .cornerRadius(10)
-                }
+        VStack {
+            Text("あなたの名前は？：\(inputText)")
+                .padding()
+            Button("名前を入力する") {
+                isShowInputView = true
+            }.sheet(isPresented: $isShowInputView) {
+                InputView(inputText: $inputText)
             }
         }
-        .frame(height: 130)
-        .background(.gray)
-        .cornerRadius(10)
-        .padding()
+    }
+}
+
+struct InputView: View {
+    @Binding var inputText: String
+    
+    var body: some View {
+        TextField("", text: $inputText)
+            .background(.gray)
+            .padding()
     }
 }
 
